0b64a4153a3b8152b31594aed5eb2d00d1980c40
shakib53626 <shakibul.ab@gmail.com>
2025-07-14 20:04:54 +0600

    Vue and inertia setup done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
 import './bootstrap';
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./**/Pages/**/*.vue', { eager: true })
+        const path  = `./${name.replace('/', '/Pages/')}.vue`
+        let   page  = pages[path]
+
+        // page.default.layout = page.default.layout || AuthLayout
+
+        if (!page) {
+            throw new Error(`Page not found: ${name} (mapped to ${path})`)
+        }
+
+        return page
+    },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el)
+  },
+})
